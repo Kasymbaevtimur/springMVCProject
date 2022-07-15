04cce7b280c451170bcf21fc94c2e0ea52a4853a
Kasymbaevtimur <105876764+Kasymbaevtimur@users.noreply.github.com>
2022-07-15 18:56:19 +0300

    sixth commit

--- src/main/java/peaksoft/entities/Course.java
+++ src/main/java/peaksoft/entities/Course.java
@@ -33,13 +33,13 @@ public class Course {
     @JoinColumn(name = "companies_id")
     private Company company;
 
-    @ManyToMany(fetch = FetchType.EAGER, cascade = {CascadeType.REFRESH, CascadeType.REMOVE, CascadeType.MERGE})
+    @ManyToMany(fetch = FetchType.LAZY, cascade = {CascadeType.REFRESH, CascadeType.MERGE,CascadeType.REMOVE})
     @JoinTable(name = "course_group",
             joinColumns = @JoinColumn(name = "courses_id"),
             inverseJoinColumns = @JoinColumn(name = "groups_id"))
     private List<Group> groups;
 
-    @OneToOne(fetch = FetchType.EAGER, cascade = {CascadeType.REFRESH, CascadeType.MERGE}, mappedBy = "course")
+    @OneToOne(fetch = FetchType.LAZY, cascade = {CascadeType.REFRESH, CascadeType.MERGE,CascadeType.REMOVE}, mappedBy = "course")
     private Teacher teacher;
 
     @Transient
--- src/main/java/peaksoft/entities/Group.java
+++ src/main/java/peaksoft/entities/Group.java
@@ -23,13 +23,13 @@ public class Group {
     @Column(name = "date_of_finish")
     private String dateOfFinish;
 
-    @ManyToMany(fetch = FetchType.EAGER, cascade = {CascadeType.REFRESH, CascadeType.MERGE})
+    @ManyToMany(fetch = FetchType.LAZY, cascade = {CascadeType.REFRESH, CascadeType.MERGE})
     @JoinTable(name = "course_group",
             joinColumns = @JoinColumn(name = "groups_id"),
             inverseJoinColumns = @JoinColumn(name = "courses_id"))
     List<Course> courses;
 
-    @OneToMany(cascade = CascadeType.ALL)
+    @OneToMany(fetch = FetchType.LAZY,cascade = {CascadeType.REMOVE,CascadeType.MERGE,CascadeType.REFRESH})
     List<Student> students;
 
     @Transient
